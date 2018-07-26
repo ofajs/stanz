b5505c08e87f3d1c008ec03343f5e20eaa1872d3
kirakiray <pikay@foxmail.com>
2018-07-26 18:21:41 +0800

    remove uparray

--- test/array_test.js
+++ test/array_test.js
@@ -5,8 +5,13 @@
         a: "aaaa",
         b: {
             val: "I am b"
+        },
+        c: {
+            arr: [1, 22, 333, 4444]
         }
     });
+
+    window.tdata = tdata;
 
     let tdata2 = stanz(tdata.toObject());
     tdata2.sync(tdata);
@@ -23,22 +28,24 @@
     tdata.unobserve(obsFucn);
 
     tdata.observe(obsFucn = e => {
-        tester.ok(e.type == "uparray", 'observe type ok 2');
-        // tester.ok(e.type == "uphost", 'observe type ok 2');
+        // tester.ok(e.type == "uparray", 'observe type ok 2');
+        tester.ok(e.type == "uphost", 'observe type ok 2');
     });
 
     tdata.watch('arr', (val, e) => {
-        tester.ok(e.type == "uparray", 'observe type ok 1');
-        // tester.ok(e.type == "uphost", 'observe type ok 1');
+        // tester.ok(e.type == "uparray", 'observe type ok 1');
+        tester.ok(e.type == "uphost", 'observe type ok 1');
     });
 
-    tdata.arr.splice(1, 0, {
+    tdata.arr.splice(1, 1, {
         aaa: "aaaaa"
     });
 
-    tester.ok(tdata.arr.length == 4, 'data array ok');
+    tester.ok(tdata.arr.length == 3, 'data array ok');
 
-    tester.ok(tdata2.arr.length == 4, 'sync array ok');
+    tester.ok(tdata2.arr.length == 3, 'sync array ok');
+
+    tdata.unobserve(obsFucn);
 
     // tdata.arr.splice(1, 0, {
     //     val: "I am new arr element"

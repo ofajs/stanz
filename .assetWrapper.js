2677a226c843da59d5b954ae73e001be72e2d81a
huang yao <kirayao@hotmail.com>
2023-04-12 13:54:04 +0800

    refactor: remove useless module

--- .assetWrapper.js
+++ .assetWrapper.js
@@ -6,11 +6,11 @@ const PACKAGE = require(path.join(CWD, "package.json"));
 module.exports = async ({ name, bundler }) => {
   if (name.split(".").pop() === "js" && bundler.options.production) {
     return {
-      header: `/* ${PACKAGE.name} - v${PACKAGE.version} ${
+      header: `// ${PACKAGE.name} - v${PACKAGE.version} ${
         PACKAGE.homepage
       }  (c) ${
         PACKAGE.startyear
-      }-${new Date().getFullYear()} ${PACKAGE.author.name} */`,
+      }-${new Date().getFullYear()} ${PACKAGE.author.name}`,
     };
   }
 };

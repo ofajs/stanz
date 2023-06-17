e84f4d356bb422f31620f085cb541bfe5b4ee6b4
Yao <kirayao@hotmail.com>
2023-06-17 15:52:23 +0800

    chore: Switching packing tools (#45)

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,47 @@
 const path = require("path");
 const CWD = process.cwd();
 const PACKAGE = require(path.join(CWD, "package.json"));
+const terser = require("@rollup/plugin-terser");
 
 const banner = `//! ${PACKAGE.name} - v${PACKAGE.version} ${
   PACKAGE.homepage
 }  (c) ${PACKAGE.startyear}-${new Date().getFullYear()} ${PACKAGE.author.name}`;
 
-module.exports = {
-  input: "src/base.mjs",
-  output: [
-    {
-      file: "dist/stanz.mjs",
-      format: "es",
-      banner,
-    },
-    {
-      file: "dist/stanz.js",
-      format: "umd",
-      name: "stanz",
-      banner,
-    },
-  ],
-  plugins: [],
-};
+module.exports = [
+  {
+    input: "src/base.mjs",
+    output: [
+      {
+        file: "dist/stanz.mjs",
+        format: "es",
+        banner,
+      },
+      {
+        file: "dist/stanz.js",
+        format: "umd",
+        name: "stanz",
+        banner,
+      },
+    ],
+    plugins: [],
+  },
+  {
+    input: "src/base.mjs",
+    output: [
+      {
+        file: "dist/stanz.min.mjs",
+        format: "es",
+        banner,
+        sourcemap: true,
+      },
+      {
+        file: "dist/stanz.min.js",
+        format: "umd",
+        name: "stanz",
+        banner,
+        sourcemap: true,
+      },
+    ],
+    plugins: [terser()],
+  },
+];

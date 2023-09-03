f5cae85d40a44312fd77315062ee8fc7ae8f2461
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-03 19:54:46 +0800

    chore: bump version

--- dist/stanz.js
+++ dist/stanz.js
@@ -1,4 +1,4 @@
-//! stanz - v8.1.21 https://github.com/kirakiray/stanz  (c) 2018-2023 YAO
+//! stanz - v8.1.22 https://github.com/kirakiray/stanz  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :

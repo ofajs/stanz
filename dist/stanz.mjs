342d1ddc9d19978f281e247bdaa1e6c01b569aa1
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-01 16:05:23 +0800

    chore: bump version

--- dist/stanz.mjs
+++ dist/stanz.mjs
@@ -1,4 +1,4 @@
-//! stanz - v8.1.22 https://github.com/kirakiray/stanz  (c) 2018-2023 YAO
+//! stanz - v8.1.23 https://github.com/kirakiray/stanz  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
